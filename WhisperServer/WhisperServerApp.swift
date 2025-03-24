89a3c01ff08bf09873e6ffa10d056938574b42c4
Pavel Frankov <pavel@frankov.ru>
2025-03-24 16:59:48 +0300

    Initial Commit

---
+++ WhisperServer/WhisperServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperServerApp.swift
+//  WhisperServer
+//
+//  Created by Frankov Pavel on 24.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct WhisperServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhisperServerTests/WhisperServerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperServerTests.swift
+//  WhisperServerTests
+//
+//  Created by Frankov Pavel on 24.03.2025.
+//
+
+import Testing
+@testable import WhisperServer
+
+struct WhisperServerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
